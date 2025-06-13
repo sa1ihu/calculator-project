ebdbe0e13ea5be7c2e144c0a98d8e408372650c1
Salihu <salihuisyakuali@gmail.com>
2025-06-13 06:28:50 +0100

    Add basic math functions and test HTML structure

---
+++ script.js
@@ -0,0 +1,32 @@
+// Basic math operations
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Main operate function
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
